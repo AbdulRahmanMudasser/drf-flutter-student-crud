182699090775c39f3975cf7f71c9e816c76e2abe
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-11-08 09:58:15 +0500

    create student view, adding url

--- backend/students/views.py
+++ backend/students/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+@api_view(['POST'])
+def create_student(request):
+    serializer = StudentSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
